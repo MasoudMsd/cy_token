ed8686d14f40cb1718b0614a4fd1b52a31cae40e
Masoud <95829809+MasoudMsd@users.noreply.github.com>
2024-10-17 15:37:26 +0200

    index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
 import { TOKEN_CONTRACT_ABI, TOKEN_CONTRACT_ADDRESS } from "../constants";
+import TokenManager from './TokenManager'; // Import TokenManager
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -10,109 +11,13 @@ export default function Home() {
   const [walletConnected, setWalletConnected] = useState(false);
   const [loading, setLoading] = useState(false);
   const [tokensToBeClaimed, setTokensToBeClaimed] = useState(zero);
-  const [balanceOfCryptoDevTokens, setBalanceOfCryptoDevTokens] =
-    useState(zero);
+  const [balanceOfCryptoDevTokens, setBalanceOfCryptoDevTokens] = useState(zero);
   const [tokenAmount, setTokenAmount] = useState(zero);
   const [tokensMinted, setTokensMinted] = useState(zero);
   const [isOwner, setIsOwner] = useState(false);
   const web3ModalRef = useRef();
 
-  const getTokensToBeClaimed = async () => {
-    const MAX_TOTAL_SUPPLY = BigInt(1000000000000000 * 10 ** 18);
-    setTokensToBeClaimed(69000000);
-  };
-
-  const getBalanceOfFCKFTokens = async () => {
-    try {
-      const provider = await getProviderOrSigner();
-      const tokenContract = new Contract(
-        TOKEN_CONTRACT_ADDRESS,
-        TOKEN_CONTRACT_ABI,
-        provider
-      );
-      const signer = await getProviderOrSigner(true);
-      const address = await signer.getAddress();
-      const balance = await tokenContract.balanceOf(address);
-      setBalanceOfCryptoDevTokens(balance);
-    } catch (err) {
-      console.error(err);
-      setBalanceOfCryptoDevTokens(zero);
-    }
-  };
-
-  const burnMemeToken = async (burnAmount) => {
-    try {
-      const signer = await getProviderOrSigner(true);
-      const tokenContract = new Contract(
-        TOKEN_CONTRACT_ADDRESS,
-        TOKEN_CONTRACT_ABI,
-        signer
-      );
-      console.log("Amount Eth: ", burnAmount);
-      const tx = await tokenContract.burn(BigInt(burnAmount * 10 ** 18));
-      setLoading(true);
-      // wait for the transaction to get mined
-      await tx.wait();
-      setLoading(false);
-      window.alert("YOU BURNED YOUR TOKENS");
-      await getBalanceOfFCKFTokens();
-      await getTotalTokensMinted();
-      await getTokensToBeClaimed();
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const getTotalTokensMinted = async () => {
-    try {
-      const provider = await getProviderOrSigner();
-      const tokenContract = new Contract(
-        TOKEN_CONTRACT_ADDRESS,
-        TOKEN_CONTRACT_ABI,
-        provider
-      );
-      // Get all the tokens that have been minted
-      const _tokensMinted = await tokenContract.totalSupply();
-      setTokensMinted(_tokensMinted);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const getOwner = async () => {
-    try {
-      const provider = await getProviderOrSigner();
-      const tokenContract = new Contract(
-        TOKEN_CONTRACT_ADDRESS,
-        TOKEN_CONTRACT_ABI,
-        provider
-      );
-      const _owner = await tokenContract.owner();
-      const signer = await getProviderOrSigner(true);
-      const address = await signer.getAddress();
-      if (address.toLowerCase() === _owner.toLowerCase()) {
-        setIsOwner(true);
-      }
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-
-  const getProviderOrSigner = async (needSigner = false) => {
-    const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.Web3Provider(provider);
-    const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 8453) {
-      window.alert("Change the network to Base Mainet");
-      throw new Error("Change network to Base Mainet");
-    }
-
-    if (needSigner) {
-      const signer = web3Provider.getSigner();
-      return signer;
-    }
-    return web3Provider;
-  };
+  // Your existing functions...
 
   const connectWallet = async () => {
     try {
@@ -136,85 +41,37 @@ export default function Home() {
       getTokensToBeClaimed();
     }
   }, [walletConnected]);
-  const renderButton = () => {
-    if (loading) {
-      return (
-        <div>
-          <button className={styles.button}>Loading...</button>
-        </div>
-      );
-    }
-    // if owner is connected, withdrawCoins() is called
-    /*if (walletConnected && isOwner) {
-      return (
-        <div>
-          <button className={styles.button1} onClick={withdrawCoins}>
-            Withdraw Coins
-          </button>
-        </div>
-      );
-    }*/
-    // If user doesn't have any tokens to claim, show the mint button
-    return (
-      <div style={{ display: "flex-col" }}>
-        <div>
-          <input
-            type="number"
-            placeholder="Amount of burn token"
-            onChange={(e) => setTokenAmount(e.target.value)}
-            className={styles.input}
-          />
-        </div>
-
-        <button
-          className={styles.button}
-          disabled={!(tokenAmount > 0)}
-          onClick={() => burnMemeToken(tokenAmount)}
-        >
-          Burn Tokens
-        </button>
-      </div>
-    );
-  };
 
   return (
     <div>
       <Head>
-        <title>Token Burner</title>
-        <meta name="description" content="burn-Dapp" />
+        <title>Token DApp</title>
+        <meta name="description" content="Token Management DApp" />
       </Head>
       <div className={styles.main}>
-        <div>
-          <h1 className={styles.title}>Welcome to BURNER DAPP!</h1>
-          {walletConnected ? (
-            <div>
-              <div className={styles.description}>
-                {/* Format Ether helps us in converting a BigNumber to string */}
-                You have minted{" "}
-                {parseFloat(
-                  utils.formatEther(balanceOfCryptoDevTokens)
-                ).toLocaleString()}{" "}
-                token
-              </div>
-              <div className={styles.description}>
-                {/* Format Ether helps us in converting a BigNumber to string */}
-                Overall{" "}
-                {parseFloat(utils.formatEther(tokensMinted)).toLocaleString()}{" "}
-                have been minted!!!
-              </div>
-              {renderButton()}
+        <h1 className={styles.title}>Welcome to the Token DApp!</h1>
+        {walletConnected ? (
+          <div>
+            <div className={styles.description}>
+              You have minted{" "}
+              {parseFloat(utils.formatEther(balanceOfCryptoDevTokens)).toLocaleString()}{" "}
+              tokens
             </div>
-          ) : (
-            <button onClick={connectWallet} className={styles.button}>
-              Connect your wallet
-            </button>
-          )}
-        </div>
+            <div className={styles.description}>
+              Overall{" "}
+              {parseFloat(utils.formatEther(tokensMinted)).toLocaleString()}{" "}
+              tokens have been minted!!!
+            </div>
+            <TokenManager /> {/* Render the TokenManager component */}
+          </div>
+        ) : (
+          <button onClick={connectWallet} className={styles.button}>
+            Connect your wallet
+          </button>
+        )}
       </div>
       <footer className={styles.footer}>
-        <p className="mt-4">
-          &copy; 2023 Meme Token. All Rights Reserved.
-        </p>
+        <p className="mt-4">&copy; 2023 Token DApp. All Rights Reserved.</p>
       </footer>
     </div>
   );
