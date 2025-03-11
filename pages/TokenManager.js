aff544420ec466ec66f5f5bb0f917088f04ca576
Masoud <95829809+MasoudMsd@users.noreply.github.com>
2025-03-11 05:54:25 +0100

    TokenManager.js

--- pages/TokenManager.js
+++ pages/TokenManager.js
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react';
-import { ethers } from 'ethers';
-import { TOKEN_CONTRACT_ADDRESS, TOKEN_ABI } from '../constants';
+
 
 const TokenManager = () => {
   const [isConnected, setIsConnected] = useState(false);
