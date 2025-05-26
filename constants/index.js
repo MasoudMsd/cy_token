315b3dfa8b4987bda2fcf11fd70934d6a1917107
Masoud <95829809+MasoudMsd@users.noreply.github.com>
2025-05-26 09:53:08 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,12 @@
+// constants/index.js
 
+export const TOKEN_CONTRACT_ADDRESS = "0x5009228dB18763984492AEE15393b149b1ea71fb";
+
+import TokenABI from './TokenABI.json';
+
+export const TOKEN_ABI = TokenABI.abi;
+
+export const TOKEN_NAME = "Solidity by Example";
 export const TOKEN_SYMBOL = "BitQueen";
 export const DECIMALS = 18;
 
