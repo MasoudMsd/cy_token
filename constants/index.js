d769fd57805db71c3cdefde10ec7d8aed9bcdacf
Masoud <95829809+MasoudMsd@users.noreply.github.com>
2025-05-26 09:52:48 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,8 @@
+// constants/index.js
+
+export const TOKEN_CONTRACT_ADDRESS = "0x5009228dB18763984492AEE15393b149b1ea71fb";
+
+import TokenABI from './TokenABI.json';
 
 export const TOKEN_ABI = TokenABI.abi;
 
