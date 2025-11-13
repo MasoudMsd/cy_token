a4222799015429e009b09113782a29836e0042b7
Masoud <95829809+MasoudMsd@users.noreply.github.com>
2025-11-13 05:36:14 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import { BigNumber, Contract, providers, utils } from "ethers";
-import Head from "next/head";
+
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
 import { TOKEN_CONTRACT_ABI, TOKEN_CONTRACT_ADDRESS } from "../constants";
